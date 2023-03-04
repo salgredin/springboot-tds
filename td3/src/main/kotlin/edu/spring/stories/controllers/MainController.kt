299af28975f03a6738eaf813525b7904074ad0bd
Victorien Gontier--Durand <22009023@etu.unicaen.fr>
2023-03-04 19:53:50 +0100

    ajout index

--- td3/src/main/kotlin/edu/spring/stories/controllers/MainController.kt
+++ td3/src/main/kotlin/edu/spring/stories/controllers/MainController.kt
@@ -4,7 +4,9 @@ import edu.spring.stories.entities.Developer
 import edu.spring.stories.entities.Story
 import edu.spring.stories.repositories.DeveloperRepository
 import edu.spring.stories.repositories.StoryRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,23 +18,27 @@ import org.springframework.web.servlet.view.RedirectView
 @Controller
 @RequestMapping("/")
 class MainController {
+    @Autowired
     lateinit var developerRepository: DeveloperRepository
+    @Autowired
     lateinit var storyRepository: StoryRepository
 
     @RequestMapping(path=["/",""])
-    fun index(): String {
+    fun index(model: MutableMap<String, Any>): String {
+        model["developers"] = developerRepository.findAll()
+        model["stories"] = storyRepository.findByDeveloperIsNull()
         return "index"
     }
     @PostMapping("/developer/add")
-    fun addDeveloper(@RequestParam developer: Developer): RedirectView {
-        developerRepository.save(developer)
+    fun addDeveloper(@RequestParam firstname: String, lastname: String): RedirectView {
+        developerRepository.save(Developer(firstname, lastname))
         return RedirectView("/")
     }
 
     @PostMapping("/developer/{id}/story")
-    fun addStory(@PathVariable id:Int, @RequestParam story: Story): RedirectView {
+    fun addStory(@PathVariable id:Int, @RequestParam story: String): RedirectView {
         developerRepository.findById(id).ifPresent {
-            it.addStory(story)
+            it.addStory(Story(story))
             developerRepository.save(it)
         }
         return RedirectView("/")
